b6cdc31f3d6ace70154adebb80ba46329f28b937
Dixman001 <dixmaneagle@gmail.com>
2022-11-28 20:02:13 -0800

    base models

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,13 @@ from datetime import datetime
 
 class BaseModel:
     ''' '''
-    self.id = str(uuid.uuid4())
-    self.created_at = datetime.now()
-    self.updated_at = datetime.now()
+
+    def __init__(self):
+        '''initializing the ins and att'''
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         '''returns string representation'''
