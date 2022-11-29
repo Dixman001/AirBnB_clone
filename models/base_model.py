cff6ad129a1c8b4fbecb399ca2a21cfd5f63d6b0
Dixman001 <dixmaneagle@gmail.com>
2022-11-29 06:23:43 -0800

    models

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     ''' '''
@@ -13,6 +14,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        storage.new(self)
 
     def __str__(self):
         '''returns string representation'''
@@ -24,6 +26,7 @@ class BaseModel:
         ''' '''
 
         self.updated_at = datetime.(now)
+        storage.save()
 
     def to_dict(self):
         '''returns a dictionary'''
