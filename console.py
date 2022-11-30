3e9d39dcc73052341ee050b98fe590d12590b8bb
Dixman001 <dixmaneagle@gmail.com>
2022-11-30 15:18:40 -0800

    console

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ from shlex import split
 
 import models
 from models.base_model import BaseModel
+from models.user import User
 
 class HBNBCommand(cmd.Cmd):
     """
---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pyton3
+"""this module initiates the user input"""
+from models.base_model import BaseModel
+
+class User:
+    """
+    email: string - empty string
+    password: string - empty string
+    first_name: string - empty string
+    last_name: string - empty string
+    """
+
+    email = ""
+    password = ""
+    first_nam = ""
+    last_name = ""
